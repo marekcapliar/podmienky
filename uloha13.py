5dad186d5391a94c314f44a9ea2a193cd9aafc01
marekcapliar <marekcapliar@gmail.com>
2022-09-18 18:56:33 +0200

    uloha13

---
+++ uloha13.py
@@ -0,0 +1,19 @@
+a=int(input("zadaj stranu a: "))
+b=int(input("zadaj stranu b: "))
+c=int(input("zadaj stranu c: "))
+
+if a+b>c and a+c>b and b+c>a:
+    if a==b==c:
+        print("trojuholnik je rovnostranny")
+    elif a==b or a==c or b==c:
+        print("trojuholnik je rovnoramenny")
+    else:
+        print("trojuholnik je roznostranny")
+    if a**2==b**2+c**2 or b**2==a**2+c**2 or c**2==a**2+b**2:
+        print("trojuhlonik je pravouhly")
+    elif a**2>b**2+c**2 or b**2>a**2+c**2 or c**2>a**2+b**2:
+        print("trojuholnik je tupouhly")
+    else:
+        print("trojuholnik je ostrouhly")
+else:
+    print("trojuholnik neexistuje")
